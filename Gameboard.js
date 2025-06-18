fb43522565209055d074006795beb34199c89807
henrique-serra <66788126+henrique-serra@users.noreply.github.com>
2025-06-18 09:24:39 -0300

    refactor: saving before refactoring code in tests

--- Gameboard.js
+++ Gameboard.js
@@ -6,8 +6,27 @@ export default class Gameboard {
     this.attackBoard = Array.from({ length: 10 }, (_) => Array.from({ length: 10 }, (_) => ({ hit: false })));
   }
 
-  placeShip(x, y, ship) {
-    const { length } = ship;
-    this.defenseBoard[y][x].ship = ship;
+  offLimits(length, row, col, horizontally = true) {
+    const rowLength = this.defenseBoard[0].length;
+    const colLength = this.defenseBoard.length;
+    if(horizontally) {
+      return (col + length) > rowLength ? true : false;
+    }
+    return (row + length) > colLength ? true : false;
+  }
+
+  placeShip(length, row, col, horizontally = true) {
+    if(this.offLimits(length, row, col, horizontally)) throw new Error('Off limits!');
+    
+    const ship = new Ship(length);
+    if(horizontally) {
+      for (let i = col; i < (col + length); i++) {
+        this.defenseBoard[row][i].ship = ship;
+      }
+    }
+
+    for (let i = row; i < (row + length); i++) {
+      this.defenseBoard[i][col].ship = ship;
+    }
   }
 }
--- Gameboard.test.js
+++ Gameboard.test.js
@@ -1,12 +1,72 @@
 import Gameboard from './Gameboard.js';
 
+let gameboard;
+
 describe('Gameboard class', () => {
-  const gameboard = new Gameboard();
+  beforeEach(() => gameboard = new Gameboard());
   test('should instantiate correctly', () => {
     expect(gameboard).toBeDefined();
     expect(Array.isArray(gameboard.defenseBoard)).toBe(true);
     expect(Array.isArray(gameboard.attackBoard)).toBe(true);
   });
 
-  describe('placeShip(0,0, new Ship(3)) should ')
+  describe('placeShip should place ships correctly', () => {
+    let length = 2;
+    let row = 0;
+    let col = 0;
+    let horizontally = true;
+    const placeShipCases = {
+      successCases: {
+        horizontally: [
+          { length: 2, row: 0, col: 0, horizontally: true },
+        ],
+        vertically: [
+
+        ]
+      },
+      errorCases: {
+        offLimits: {
+          horizontally: [
+
+          ],
+          vertically: [
+
+          ]
+        },
+        typeError: [
+
+        ]
+      }
+    }
+    test('horizontally', () => {
+      gameboard.placeShip(length, row, col, horizontally);
+      const [firstRow] = gameboard.defenseBoard;
+      expect(firstRow[0].ship).toBeDefined();
+      expect(firstRow[1].ship).toBeDefined();
+    });
+
+    test('vertically', () => {
+      horizontally = false;
+      gameboard.placeShip(length, row, col, horizontally);
+      const [firstRow, secondRow] = gameboard.defenseBoard;
+      expect(firstRow[0].ship).toBeDefined();
+      expect(secondRow[0].ship).toBeDefined();
+    });
+
+    describe('off limits should throw', () => {
+      test('off limits horizontally', () => {
+        row = 0;
+        col = 9;
+        horizontally = true;
+        expect(() => gameboard.placeShip(length, row, col, horizontally)).toThrow('Off limits!');
+      });
+
+      test('off limits vertically', () => {
+        row = 9;
+        col = 0;
+        horizontally = false;
+        expect(() => gameboard.placeShip(length, row, col, horizontally)).toThrow('Off limits!');
+      })
+    })
+  })
 })
