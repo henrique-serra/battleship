742b254778edf9e68dc6f7d1c187d9dda2816f50
henrique-serra <66788126+henrique-serra@users.noreply.github.com>
2025-06-25 09:51:28 -0300

    fix: add .js to imports

--- Controller.js
+++ Controller.js
@@ -1,4 +1,4 @@
-import Player from "./Player";
+import Player from "./Player.js";
 
 class Controller {
   constructor(player1 = new Player(), player2 = new Player('Player 2', 'computer')) {
--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import GameUI from "./ui/GameUI";
+import GameUI from "./ui/GameUI.js";
 
 document.addEventListener('DOMContentLoaded', () => {
   new GameUI();
