b7451a8280769f740e1a16db4d0a43d4a5289907
henrique-serra <66788126+henrique-serra@users.noreply.github.com>
2025-07-03 16:52:04 -0300

    checkpoint to undo refactoring if necessary

--- Ship.js
+++ Ship.js
@@ -3,30 +3,60 @@ export default class Ship {
     if (typeof length !== 'number' || !Number.isInteger(length)) throw new Error('length must be an integer');
     if (length < 1 || length > 5) throw new Error('length must be between 1 and 5');
     
-    this.length = length;
-    this.type = this.getShipType(length);
+    // this.length = length;
+    // this.shipTypes = {
+    //   1: 'patrol',
+    //   2: 'submarine',
+    //   3: 'destroyer',
+    //   4: 'battleship',
+    //   5: 'carrier',
+    // };
+
+    this.shipCatalog = [
+      {
+        name: 'patrol',
+        length: 1,
+        emoji: '⛵',
+      },
+      {
+        name: 'submarine',
+        length: 2,
+        emoji: '🚤',
+      },
+      {
+        name: 'destroyer',
+        length: 3,
+        emoji: '🛥️',
+      },
+      {
+        name: 'battleship',
+        length: 4,
+        emoji: '🚢',
+      },
+      {
+        name: 'carrier',
+        length: 5,
+        emoji: '🛳️',
+      },
+    ];
+
+    this.shipInfo = this.shipCatalog[length];
+    // this.type = this.getShipType(length);
+    // this.emoji = ;
     this.hits = hits;
     this.sunk = sunk;
     this.positions = [];
   }
 
-  getShipType(length) {
-    const shipTypes = {
-      1: 'patrol',
-      2: 'submarine',
-      3: 'destroyer',
-      4: 'battleship',
-      5: 'carrier',
-    };
-
-    return shipTypes[length];
-  }
+  // getShipType(length) {
+  //   return this.shipTypes[length];
+  // }
 
   hit() {
     this.hits += 1;
   }
 
   isSunk() {
-    return this.hits === this.length ? true : false;
+    return this.hits === this.shipInfo.length ? true : false;
   }
 }
